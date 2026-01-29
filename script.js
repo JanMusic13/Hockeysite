b9a833c44cb5250149354b157cc8fa102d2b82a5
JanMusic13 <brave.janmusic@gmail.com>
2026-01-29 18:17:22 +0100

    Intersection obesrver fix spelling

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const slike = document.querySelector('.slike');
 const observer = new IntersectionObserver((entries) => {
-    if (entrires[0].isIntersecting) {
+    if (entries[0].isIntersecting) {
         entries[0].target.classList.add("show")
     }
     else {
