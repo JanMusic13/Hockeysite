2263ad4054a1e5960b9a668bb5f8dd5e7b4860f3
JanMusic13 <brave.janmusic@gmail.com>
2026-01-29 18:14:40 +0100

    Intersection obesrver mid

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const slike = document.querySelector('.slike');
 const observer = new IntersectionObserver((entries) => {
-    console.log(entries);
+    if (entrires[0].isIntersecting) {
+        entries[0].target.classList.add("show")
+    }
+    else {
+        entries[0].target.classList.remove("show")
+    }
 }, {})
 
 observer.observe(slike)
