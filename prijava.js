dc3b0ea9ae6471329cac786fadcbca4de1d5e5a2
JanMusic13 <brave.janmusic@gmail.com>
2026-02-05 14:09:28 +0100

    Javascript za height

--- prijava.js
+++ prijava.js
@@ -1,6 +1,13 @@
 const submit = document.getElementById("submit");
 
+function lockHeight() {
+    // Izmeri dejansko višino v pikslih ob nalaganju
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
 
+window.addEventListener('load', lockHeight);
+// Ne dodajaj 'resize' eventa, ker prav to povzroča tvoj problem!
 submit.addEventListener("click", (e) => {
     const email = document.querySelector(".mail");
     const tel = document.querySelector(".tele");
